007f9c0e710a0b2f37b5bc714c7913d1e81d3e96
gilbertodevx <gmrf1996@gmail.com>
2022-07-26 17:09:41 +0100

    Readme updated

--- src/store/redux-store.ts
+++ src/store/redux-store.ts
@@ -1,10 +1,7 @@
-// import { createStore } from 'redux';
 import { createSlice, configureStore, PayloadAction } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
   selectedCountries: [],
-  // selectedCountries: [{ country: null, isChecked: false }],
-  // isChecked: false,
 };
 
 const selectedCountriesSlice = createSlice({
@@ -19,20 +16,9 @@ const selectedCountriesSlice = createSlice({
         (item: any) => item.name.common === action.payload.country.name.common
       );
 
-      // const isFilter = state.selectedCountries?.map(
-      //   (item: any) =>
-      //     // item.country.includes(action.payload.nameCheckbox)
-      //     item
-      // );
       if (!existsCountryInFavorite && action.payload.isChecked) {
         state.selectedCountries.push(action.payload.country);
-        // return {
-        //   ...state,
-        //   selectedCountries: [...state.selectedCountries],
-        //   // isChecked: action.payload.isChecked,
-        // };
       }
-      // return state;
     },
     removeCountriesChecked(state, action) {
       // FILTER
@@ -42,74 +28,15 @@ const selectedCountriesSlice = createSlice({
       );
 
       state.selectedCountries = [...isFilter];
-
-      //   console.log(isFilter);
       console.log(
         'REMOVE - isChecked:' + action.payload.isChecked + ' FALSE - '
       );
-
-      // return {
-      //   ...state,
-      //   selectedCountries: [...isFilter],
-      // };
     },
   },
 });
 
-// const reducer = (state: any = INITIAL_STATE, action: any) => {
-//   if (action.type === 'ADD_COUNTRIES_CHECKED') {
-//     // console.log(state);
-//     // return {
-//     //   ...state,
-//     //   selectedCountries: [...state.selectedCountries, { ...action.payload }],
-//     // };
-//     console.log(
-//       'ADD - isChecked:' +
-//         action.payload.isChecked +
-//         ' - ' +
-//         action.payload.country.name.common
-//     );
-
-//     const existsCountryInFavorite = state.selectedCountries?.some(
-//       (item: any) => item.name.common === action.payload.country.name.common
-//     );
-
-//     // const isFilter = state.selectedCountries?.map(
-//     //   (item: any) =>
-//     //     // item.country.includes(action.payload.nameCheckbox)
-//     //     item
-//     // );
-//     if (!existsCountryInFavorite && action.payload.isChecked) {
-//       state.selectedCountries.push(action.payload.country);
-//       return {
-//         ...state,
-//         selectedCountries: [...state.selectedCountries],
-//         // isChecked: action.payload.isChecked,
-//       };
-//     }
-
-//     return state;
-//   }
-
-//   if (action.type === 'REMOVE_COUNTRIES_CHECKED') {
-//     // FILTER
-//     const isFilter = state.selectedCountries?.filter(
-//       (country: any) =>
-//         country.name.common !== action.payload.country.name.common
-//     );
-//     //   console.log(isFilter);
-//     console.log('REMOVE - isChecked:' + action.payload.isChecked + ' FALSE - ');
-//     return {
-//       ...state,
-//       selectedCountries: [...isFilter],
-//     };
-//   }
-//   return state;
-// };
 export const selectedCountriesActions = selectedCountriesSlice.actions;
 
-/////////////////////////////////////////////////////////////
-// export const store = createStore(reducer);
 export const store = configureStore({
   reducer: selectedCountriesSlice.reducer,
 });
